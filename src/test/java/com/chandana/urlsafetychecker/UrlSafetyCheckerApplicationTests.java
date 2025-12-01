13628b807a119eba1c7fe5ead33291b7a1176665
Chandana <vchandana2304@gmail.com>
2025-12-01 14:10:36 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/chandana/urlsafetychecker/UrlSafetyCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chandana.urlsafetychecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlSafetyCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
